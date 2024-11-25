3235a1007919e0e63c1b116faea40f228a46294c
jrothlander <50993816+jrothlander@users.noreply.github.com>
2024-11-24 19:37:02 -0600

    Add files via upload

---
+++ AITechniquesForGameProgramming_PremierPress_2002/VisualStudio2022/Chapter05/ImprovedTSP/resource1.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ImprovedTSP.rc
+//
+#define IDR_MENU1                       101
+#define ID_CROSSOVERTYPE_PERMUTATION    40001
+#define ID_CROSSOVERTYPE_ORDERBASED     40002
+#define ID_CROSSOVERTYPE_POSITIONBASED  40003
+#define ID_MUTATIONTYPE_EXCHANGE        40004
+#define ID_MUTATIONTYPE_SCRAMBLE        40005
+#define ID_MUTATIONTYPE_INSERTION       40006
+#define ID_MUTATIONTYPE_DISPLACEMENT    40007
+#define ID_SCALETYPE_NONE               40008
+#define ID_SCALETYPE_RANK               40009
+#define ID_SCALETYPE_SIGMA              40010
+#define ID_SCALETYPE_BOLTZMANN          40011
+#define ID_SELECTIONMETHOD_ROULETTE     40012
+#define ID_SELECTIONMETHOD_TOURNY       40013
+#define ID_SELECTIONMETHOD_ALTTOURNY    40014
+#define ID_SELECTIONMETHOD_SUS          40015
+#define ID_ELITISM_ON                   40016
+#define ID_ELITISM_OFF                  40017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40018
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
