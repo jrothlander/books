856f3f9de8c56284b176f04e0ef81177446baf40
jrothlander <50993816+jrothlander@users.noreply.github.com>
2024-11-24 19:35:45 -0600

    Add files via upload

---
+++ AITechniquesForGameProgramming_PremierPress_2002/VisualStudio2022/Chapter04/TSP/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TSP.rc
+//
+#define IDR_MENU1                       101
+#define ID_CROSSOVERTYPE_MUTATIONTYPE   40001
+#define ID_MUTATIONTYPE_EXCHANGE        40002
+#define ID_MUTATIONTYPE_SCRAMBLE        40003
+#define ID_MUTATIONTYPE_INSERTION       40004
+#define ID_MUTATIONTYPE_DISPLACEMENT    40005
+#define ID_CROSSOVERTYPE_SCALETYPE      40006
+#define ID_SCALETYPE_NORMAL             40007
+#define ID_SCALETYPE_RANK               40008
+#define ID_SCALETYPE_LINEAR             40009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
