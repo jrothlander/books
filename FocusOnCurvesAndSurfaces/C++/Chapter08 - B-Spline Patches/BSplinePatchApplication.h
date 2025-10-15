72a958a1f4c98ce86ffe799a32048ab56615d697
jrothlander <50993816+jrothlander@users.noreply.github.com>
2025-10-15 17:43:01 -0500

    Add files via upload

---
+++ FocusOnCurvesAndSurfaces/C++/Chapter08 - B-Spline Patches/BSplinePatchApplication.h
@@ -0,0 +1,53 @@
+// PolyPatchApplication.h: interface for the CPolyPatchApplication class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_POLYPatchAPPLICATION_H__C7493EF7_0AD0_437F_BF31_2C2B1615F315__INCLUDED_)
+#define AFX_POLYPatchAPPLICATION_H__C7493EF7_0AD0_437F_BF31_2C2B1615F315__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "PatchApplication.h"
+
+#define NUM_PATCH_VERTICES 15
+
+#define NUM_U_POINTS 5
+#define NUM_V_POINTS 5
+
+#define PATCH_ORDER_U  3
+#define PATCH_ORDER_V  3
+
+class CBSplinePatchApplication : public CPatchApplication  
+{
+public:
+	void DrawBasisFunctions();
+	void DefineBasisFunctions();
+	void SetKnotVectors();
+
+	CBSplinePatchApplication();
+	virtual ~CBSplinePatchApplication();
+
+	BOOL FillPatchBuffer();
+
+	virtual BOOL PostInitialize();
+	virtual BOOL PreTerminate();
+	virtual void Render();
+
+	float        m_KnotVectorU[NUM_U_POINTS + PATCH_ORDER_U];
+	float        m_KnotVectorV[NUM_V_POINTS + PATCH_ORDER_V];
+
+	//This 3D array is layed out as follows
+	//[Values of t for each vertex][Resulting Basis function[i][k] for the value of t]
+	float        m_BasisFunctionsU[NUM_PATCH_VERTICES][NUM_U_POINTS][PATCH_ORDER_U];
+	float        m_DerivativeBasisU[NUM_PATCH_VERTICES][NUM_U_POINTS][PATCH_ORDER_U];
+
+	float        m_BasisFunctionsV[NUM_PATCH_VERTICES][NUM_V_POINTS][PATCH_ORDER_V];
+	float        m_DerivativeBasisV[NUM_PATCH_VERTICES][NUM_V_POINTS][PATCH_ORDER_V];
+
+	LPDIRECT3DVERTEXBUFFER8 m_pPatchVertices;
+	LPDIRECT3DINDEXBUFFER8  m_pPatchIndex;
+};
+
+#endif // !defined(AFX_POLYPatchAPPLICATION_H__C7493EF7_0AD0_437F_BF31_2C2B1615F315__INCLUDED_)
