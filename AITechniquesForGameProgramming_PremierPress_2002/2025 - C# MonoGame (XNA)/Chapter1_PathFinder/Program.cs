0ef18a8947ed4ca8215082bc04baf527ddf7c103
jrothlander <50993816+jrothlander@users.noreply.github.com>
2025-11-30 11:45:30 -0600

    Add files via upload

---
+++ AITechniquesForGameProgramming_PremierPress_2002/2025 - C# MonoGame (XNA)/Chapter1_PathFinder/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Section_AI_1_1_Pathfinder.Game1();
+game.Run();
---
+++ AITechniquesForGameProgramming_PremierPress_2002/2025 - C# MonoGame (XNA)/Chapter1_PathFinder/Utilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_AI_1_1_Pathfinder
+{
+    public class Utilities
+    {
+
+        private readonly Random _random = new();
+        private const int RAND_MAX = 0x7fff;
+
+        public int RandInt(int x, int y)
+        {
+            return _random.Next(x, y) % (y - x + 1) + x;
+        }
+
+        public double RandFloat()
+        {
+            return _random.NextDouble() / (RAND_MAX + 1.0);
+        }
+
+        //public bool RandBool()
+        //{
+        //    return Convert.ToBoolean(RandInt(0, 1));
+        //}
+
+        //public double RandClamped()
+        //{
+        //    return RandFloat() - RandFloat();
+        //}
+
+    }
+}
